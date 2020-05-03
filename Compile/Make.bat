93c51e0cad4cd9204c0b4ba77fc6b9a892c5066d
z-h-sun <zehaosun97@gmail.com>
2020-05-02 23:25:08 -0400

    switch backend; enable Ctrl-C; add more functionalities

--- Compile/Make.bat
+++ Compile/Make.bat
@@ -1,2 +1,2 @@
 set PATH=C:\mingw64\bin;%PATH%
-nuitka --mingw64 --show-progress --show-memory --standalone --enable-plugin=tk-inter --enable-plugin=multiprocessing --recurse-all --recurse-not-to=numpy,matplotlib --windows-icon="%~dp0\1.ico" ../nicsa
+nuitka --mingw64 --show-progress --show-memory --show-modules --standalone --enable-plugin=multiprocessing --recurse-all --recurse-not-to=numpy,matplotlib --windows-icon="%~dp0\1.ico" ../nicsa
